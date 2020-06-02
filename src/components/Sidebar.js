5613870c3d424308f1cd33f14acd09c964dd0472
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-06-02 14:14:30 -0400

    added styling and logic for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import { CardHeading } from 'react-bootstrap-icons';
 import sections from './sections';
+import Select from 'react-select';
 
 const Sidebar = ({ styles }) => {
-    const sidebarStyle = {
+    const [isOpen, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(!isOpen);
+
+    const sidebarStyleClosed = {
         height: "100vh",
         width: styles.sidebarWidth,
+        position: "fixed",
+        backgroundColor: "#333",
+        paddingTop: 40
+    };
+    const sidebarStyleOpen = {
+        height: "100vh",
+        width: styles.sidebarWidth * 6,
         position: "fixed",
         backgroundColor: "#333",
         paddingTop: 40
@@ -32,13 +44,21 @@ const Sidebar = ({ styles }) => {
         marginBottom: 60,
         fontWeight: "bold"
     };
-    const handleOpenSidebar = () => {
-
-    }
 
     return (
-        <div style={sidebarStyle}>
-            <Button style={{ width: styles.sidebarWidth }} onClick={() => handleOpenSidebar}><CardHeading style={iconStyle} color='white' /> </Button>
+        <div style={isOpen ? sidebarStyleOpen : sidebarStyleClosed}>
+            <Button style={{ width: styles.sidebarWidth }} onClick={() => handleOpen()}><CardHeading style={iconStyle} color='white' /> </Button>
+            {isOpen ?
+                Object.keys(sections).map((key, index) => {
+                    return (
+                        Object.keys(sections[key]).map(k => {
+                            return (
+                                <Select options={sections[key][k]} />
+                            );
+                        })
+                    );
+                }) :
+                ""}
 
         </div>
     );
